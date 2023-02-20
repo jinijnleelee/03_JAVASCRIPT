aa3c1486bb18c60b7ba29fffaef2a6a0269029d9
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-02-20 15:09:40 +0900

    js 연습

---
+++ js/01_개요.js
@@ -0,0 +1,16 @@
+// 한줄 주석
+/* 범위 주석 */
+//  js 파일은 <script>그 내부라고 생각하면 됨
+
+function btnClick2() {
+    alert("external 버튼이 클림됨!");
+}
+
+function changeColor1() {
+document.getElementById("box").style.backgroundColor = "red";
+}
+
+
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+    }
