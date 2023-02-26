802ed1f2701037aafa6d2b0fcf2a4f7c3da24932
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-02-26 23:48:12 +0900

    과제

---
+++ js/공포게임.js
@@ -0,0 +1,24 @@
+
+
+const mouse = document.getElementById("mouse");
+const love = document.getElementById("love");
+
+
+
+var count = 0;
+
+
+
+mouse.addEventListener("mouseover", function() {
+
+	if (count < 10) {
+        love.innerHTML += "<div id='love'><img src='/img/캡처2.PNG' ></div>";
+        count++;
+    } else {
+        love.innerHTML = "<div id='love'><img src='/img/캡처3.PNG' ></div>";
+    }
+});
+
+  
+       
+   
