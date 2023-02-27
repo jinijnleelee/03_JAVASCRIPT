1468e55620f23fb1f3a19291e40bc4eefe2a7589
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-02-28 00:21:21 +0900

    js실습

--- js/09_배열.js
+++ js/09_배열.js
@@ -1,34 +1,34 @@
-// //배열확인
-// document.getElementById("btn1").addEventListener("click", function  {
+//배열확인
+document.getElementById("btn1").addEventListener("click", function () {
 
-//     const arr1 = new Array();
-//     const arr2 = new Array(3);
-//     const arr3 = [];
-//     const arr4 = ['사과','딸기','바나나'];
+    const arr1 = new Array();
+    const arr2 = new Array(3);
+    const arr3 = [];
+    const arr4 = ['사과','딸기','바나나'];
 
-//     console.log(arr1)
-//     console.log(arr2)
-//     console.log(arr3)
-//     console.log(arr4)
+    console.log(arr1)
+    console.log(arr2)
+    console.log(arr3)
+    console.log(arr4)
 
-//     //배열에 존재하지 않는 인덱스에 값 대입
+    //배열에 존재하지 않는 인덱스에 값 대입
+   // -> 자동으로 추가되면서 길이 증가
 
+    arr1[0] = "김밥";
+    arr1[1] = 5000;
+    arr1[2] = true;
 
-//     arr1[0] = "김밥";
-//     arr1[1] = 5000;
-//     arr1[2] = true;
-
-//     // 중간 인덱스 건너뛰면, 건더 뛴 부분은 empty로 남음
-//     arr1[5] = 111; //추가 기능
-//     console.log(arr1)
+    // 중간 인덱스 건너뛰면, 건더 뛴 부분은 empty로 남음
+    arr1[5] = 111; //추가 기능
+    console.log(arr1)
 
 //for문으로 배열 요소 반복 접근하기
 
 //1.일반 for 문 - 배열 ,컬렉션
-// for(let i = 0; i < arr4.length; i ++){
+//  for(let i = 0; i < arr4.length; i ++){
 //     console.log(arr4[i]);
 
-// }
+//  }
 
 // 2. 배열 .forEach(function(item,index ) {반복수행 코드  })  - 배열
 // //--item  : 현재 접근중인 요소
@@ -36,13 +36,13 @@
 
 // * 여러요소를 얻어온 경우 (HTMLCollection , NodeList)는
 // 배열이 아니므로 forEach()문 쓸수없다
-// arr4.forEach( function (a,i) {
-// console.log(i + ":" + a)
-
-
+//  arr4.forEach( function (a,i) {
+//  console.log(i + ":" + a)
 //  })
 
-//3. for(item of 배열(또는 콜렉션)) {} - 배여르 컬렉션
+})
+//3. for(item of 배열(또는 콜렉션)) {} - 배열 컬렉션
+//(symbol.iterator 가 존재하는 객체에 사용가능 )
 // == java 향상된 for문과비슷하게 생김
 //     for(let item of arr4) {
 //     console.log(itme)
@@ -63,7 +63,7 @@
 //         sum += Number(item.innerText)
 
 //     }
-
+    console.log("sum :  " + sum)
 
 // //4.for (let key in 객체 ) - js 객채용 for문
 // })
@@ -83,6 +83,7 @@
 // arr.push("재");
 
 // console.log(arr.toString());
+
 
 // const temp = arr.pop();
 // console.log(temp);
--- js/회원가입양식.js
+++ js/회원가입양식.js
@@ -4,26 +4,43 @@ document.getElementById("inputId").addEventListener("keyup", function() {
     
     const regEx = /^[a-z0-9]{5,}$/;
 
-    const result3 = document.getElementById("result1");
+    const result1 = document.getElementById("result1");
 
-    // 요소.classList : 요소가 가지고 있는 클래스를 배열로 반환
-    // 요소.classList.remove("클래스명") : 요소의 특정 클래스 제거
-    // 요소.classList.add("클래스명") : 요소의 특정 클래스 추가
-    // 요소.classList.toggle("클래스명") : 클래스가 있으면 제거 없으면 추가
 
     if(regEx.test(this.value)) {
-        result3.innerText = "유효한 형식 입니다."
+        result1.innerText = "사용가능."
 
-        result3.classList.add("confirm1");
-        result3.classList.remove("error");
+        result1.classList.add("confirm1");
+        result1.classList.remove("error");
         
     } else {
-        result3.innerText = "잘못된 형식 입니다."
-        result3.classList.add("error");
-        result3.classList.remove("confirm1");
+        result1.innerText = "불가능."
+        result1.classList.add("error");
+        result1.classList.remove("confirm1");
     }
 })
 
+// 비밀번호 유효성 검사
+document.getElementById("inputPw").addEventListener("keyup", function() {
+
+    
+    // const regEx =
+
+    // const result2 = document.getElementById("result2");
+
+
+    if(regEx.test(this.value)) {
+        result2.innerText = "사용가능."
+
+        result2.classList.add("confirm2");
+        result2.classList.remove("error");
+        
+    } else {
+        result2.innerText = "불가능."
+        result2.classList.add("error");
+        result2.classList.remove("confirm2");
+    }
+})
 
 
 // 이름 유효성 검사
@@ -34,19 +51,15 @@ document.getElementById("inputName").addEventListener("keyup", function() {
 
     const result3 = document.getElementById("result3");
 
-    // 요소.classList : 요소가 가지고 있는 클래스를 배열로 반환
-    // 요소.classList.remove("클래스명") : 요소의 특정 클래스 제거
-    // 요소.classList.add("클래스명") : 요소의 특정 클래스 추가
-    // 요소.classList.toggle("클래스명") : 클래스가 있으면 제거 없으면 추가
 
     if(regEx.test(this.value)) {
-        result3.innerText = "유효한 형식 입니다."
+        result3.innerText = "사용가능."
 
         result3.classList.add("confirm3");
         result3.classList.remove("error");
         
     } else {
-        result3.innerText = "잘못된 형식 입니다."
+        result3.innerText = "불가능."
         result3.classList.add("error");
         result3.classList.remove("confirm3");
     }
@@ -56,23 +69,20 @@ document.getElementById("inputName").addEventListener("keyup", function() {
 document.getElementById("inputPhone").addEventListener("keyup", function() {
 
     // 010으로 시작. 숫자만 입력
-    const regEx = /^010\d/;
+    const regEx = /^010[0-9]{4,}[0-9]{4,}$/;
 
     const result4 = document.getElementById("result4");
 
-    // 요소.classList : 요소가 가지고 있는 클래스를 배열로 반환
-    // 요소.classList.remove("클래스명") : 요소의 특정 클래스 제거
-    // 요소.classList.add("클래스명") : 요소의 특정 클래스 추가
-    // 요소.classList.toggle("클래스명") : 클래스가 있으면 제거 없으면 추가
+   
 
     if(regEx.test(this.value)) {
-        result4.innerText = "유효한 형식 입니다."
+        result4.innerText = "사용가능."
 
         result4.classList.add("confirm4");
         result4.classList.remove("error");
         
     } else {
-        result4.innerText = "잘못된 형식 입니다."
+        result4.innerText = "불가능"
         result4.classList.add("error");
         result4.classList.remove("confirm4");
     }
@@ -82,50 +92,43 @@ document.getElementById("inputPhone").addEventListener("keyup", function() {
 // 주민번호 유효성 검사
 document.getElementById("inputYear").addEventListener("keyup", function() {
 
-    // 010으로 시작. 숫자만 입력
-    const regEx = /^0-9/;
+    
+    const regEx = /^[0-9]{6,}([-? ?]?)[1-4]{2,}[0-9]{5,}$/;
 
     const result5 = document.getElementById("result5");
 
-    // 요소.classList : 요소가 가지고 있는 클래스를 배열로 반환
-    // 요소.classList.remove("클래스명") : 요소의 특정 클래스 제거
-    // 요소.classList.add("클래스명") : 요소의 특정 클래스 추가
-    // 요소.classList.toggle("클래스명") : 클래스가 있으면 제거 없으면 추가
 
     if(regEx.test(this.value)) {
-        result5.innerText = "유효한 형식 입니다."
+        result5.innerText = "사용가능."
 
         result5.classList.add("confirm5");
         result5.classList.remove("error");
         
     } else {
-        result5.innerText = "잘못된 형식 입니다."
+        result5.innerText = "불가능."
         result5.classList.add("error");
         result5.classList.remove("confirm5");
     }
 })
-document.getElementById("inputYear").addEventListener("keyup", function() {
 
-    // 010으로 시작. 숫자만 입력
-    const regEx = /^010\d/;
+// 이메일 유효성 검사
+document.getElementById("inputEmail").addEventListener("keyup", function() {
 
-    const result5 = document.getElementById("result5");
+    //이메일(@무조건 포함. 맨뒤는 .com으로 끝날것) 
+    const regEx = /^[a-zA-Z0-9]{3,}@[a-z]{3,}(.com\b)$/;
 
-    // 요소.classList : 요소가 가지고 있는 클래스를 배열로 반환
-    // 요소.classList.remove("클래스명") : 요소의 특정 클래스 제거
-    // 요소.classList.add("클래스명") : 요소의 특정 클래스 추가
-    // 요소.classList.toggle("클래스명") : 클래스가 있으면 제거 없으면 추가
+    const result6 = document.getElementById("result6");
+
 
     if(regEx.test(this.value)) {
-        result5.innerText = "유효한 형식 입니다."
+        result6.innerText = "사용가능."
 
-        result5.classList.add("confirm5");
-        result5.classList.remove("error");
+        result6.classList.add("confirm6");
+        result6.classList.remove("error");
         
     } else {
-        result5.innerText = "잘못된 형식 입니다."
-        result5.classList.add("error");
-        result5.classList.remove("confirm5");
+        result6.innerText = "불가능."
+        result6.classList.add("error");
+        result6.classList.remove("confirm6");
     }
 })
-
