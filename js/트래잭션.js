3e1ab9af535cf11656a192ca74ff03a3858c7eed
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-03-01 02:34:59 +0900

    과제

---
+++ js/트래잭션.js
@@ -0,0 +1,53 @@
+
+
+
+const key = document.querySelectorAll("td");
+
+const input = document.querySelectorAll("input");
+
+function fnInput(){
+
+
+
+    for(let i = 0; input.length; i++){
+        input[i];
+
+    }
+
+}
+function fnKey(){
+
+
+
+    for(let i = 0; key.length; i++){
+        key[i].style.backgroundColor = fnInput().value;
+    
+    }
+}
+
+
+
+
+
+
+fnInput().addEventListener("keyup", function () {
+        //박스 요소 선택 
+        
+       
+
+        
+
+
+
+
+
+          
+       
+    
+ })
+
+
+
+
+
+
